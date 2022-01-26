c1ac77cce39f97aa6f89df30b9922c2048adc5bd
Teresa Wehmeier <88757040+TeresaWehmeier@users.noreply.github.com>
2022-01-25 21:40:11 -0600

    load map with layers to single point branh

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437], 10);
 
 // add a marker to the map for Los Angeles, California
 let marker = L.circleMarker([34.0522, -118.2437], {
